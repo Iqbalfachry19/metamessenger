a1c7de372d3c4b489c311d3b6eb75a9c41c00162
iqbalfachry19 <ifachry@rocketmail.com>
2022-11-12 09:30:24 +0700

    hi

--- pusher.ts
+++ pusher.ts
@@ -3,7 +3,7 @@ import ClientPusher from 'pusher-js';
 export const serverPusher = new Pusher({
   appId: '1505533',
   key: '49a276f4b4ec5986bd04',
-  secret: 'cb635df6e1fe8d3d420e',
+  secret: process.env.PUSHER_SECRET!,
   cluster: 'ap1',
   useTLS: true,
 });
